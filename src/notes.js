8cf497bc2393eb76c8ab7fa4b868423531687fe2
RivaldoPardede <rivaldopardede3@gmail.com>
2023-05-05 21:28:17 +0700

    delete notes object

--- src/notes.js
+++ src/notes.js
@@ -1,14 +1,3 @@
-const { nanoid } = require('nanoid');
-
-const notes = [
-  {
-    id: nanoid(16),
-    title: 'Belajar Javascript',
-    createdAt: new Date().toISOString(),
-    updatedAt: new Date().toISOString(),
-    tags: ['javascript', 'programming'],
-    body: 'Belajar Javascript itu menyenangkan',
-  },
-];
+const notes = [];
 
 module.exports = notes;
